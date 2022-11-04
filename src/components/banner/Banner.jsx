fb60f6923ae5a9eb2b24f19298abeea3e4c12240
citrameiikaa <83491427+citrameiikaa@users.noreply.github.com>
2022-11-04 18:09:47 +0700

    Update Banner.jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
         className="p-5 d-flex flex-column align-items-center justify-content-center"
       >
         <h1 className="mt-4 mb-2 text-white fw-bold text-center">
-          Sewa Mobil di Surabaya Sekarang
+          Sewa Mobil di Palembang Sekarang
         </h1>
         <p className="text-white text-center mb-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
